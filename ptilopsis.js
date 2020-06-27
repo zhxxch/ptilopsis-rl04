dae1beef80190029d02202d3375a375e969f0c8b
ZXC <zhxxch@outlook.com>
2020-06-27 10:26:02 +0800

    CQBW

--- ptilopsis.js
+++ ptilopsis.js
@@ -1,9 +1,9 @@
 const playername = "ptilopsis";
-const id2 = "build_char_128_plosis"
+const id2 = "enemy_1504_cqbw"
 var lastFrameTime = Date.now() / 1000;
 var canvas = document.querySelector("canvas");
-canvas.width=Math.min(200,window.innerWidth);
-canvas.height=Math.min(200,window.innerHeight);
+canvas.width=Math.min(512,window.innerWidth);
+canvas.height=Math.min(512,window.innerHeight);
 var config = { alpha: true };
 var gl = canvas.getContext("webgl", config) || canvas.getContext("experimental-webgl", config);
 if (!gl) {
@@ -11,6 +11,7 @@ if (!gl) {
 }
 renderer = new spine.webgl.SceneRenderer(canvas, gl);
 var skeletonRenderer = new spine.webgl.SkeletonRenderer(gl);
+skeletonRenderer.premultipliedAlpha=0;
 var shapes = new spine.webgl.ShapeRenderer(gl);
 var state, skeleton, bounds;
 var assetManager = new spine.webgl.AssetManager(gl);
@@ -31,23 +32,26 @@ async function loadskel(skeljson) {
 	bounds = calculateBounds(skeleton);
 	renderer.camera.position.x = bounds.offset.x+bounds.size.x/2;
 	renderer.camera.position.y = bounds.offset.y+bounds.size.y/2;
-	renderer.camera.viewportWidth = bounds.size.x * 1.2;
-	renderer.camera.viewportHeight = bounds.size.y * 1.2;
+	renderer.camera.viewportWidth = 512*(512/bounds.size.x);
+	renderer.camera.viewportHeight = 512*(512/bounds.size.y);
 	renderer.resize(spine.webgl.ResizeMode.Fit);
 	var animationStateData = new spine.AnimationStateData(skeleton.data);
 	var animationState = new spine.AnimationState(animationStateData);
-	animationState.setAnimation(0, "Relax", true);
+	animationState.setAnimation(0, "Die", false);
+	animationState.addAnimation(0, "Default", true);
 	state = animationState;
 	if(Math.random()<0.5){skeleton.flipX = !skeleton.flipX;}
-	requestAnimationFrame(render);
+	var now = Date.now() / 1000;
+	lastFrameTime = now;
+	//requestAnimationFrame(render);
 }
 function render() {
 	var now = Date.now() / 1000;
 	var delta = now - lastFrameTime;
-	delta = 0.016;
+	//delta=0.016
 	lastFrameTime = now;
 
-	gl.clearColor(1, 1, 1, 0);
+	gl.clearColor(0, 0, 0, 0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
 	// Apply the animation state based on the delta time.
@@ -60,20 +64,47 @@ function render() {
 	renderer.end();
 	requestAnimationFrame(render);
 }
-document.querySelector("canvas").addEventListener("click",interact);
-var voice = document.querySelector("audio");
-var lastInteractTime = 0;
-function interact(){
-	var now = Date.now()/1000;
-	const delta = now-lastInteractTime;
-	voice.currentTime=0;
-	voice.play();
-	if(delta>1){
-		lastInteractTime=now;
-	state.setAnimation(0,"Interact",false,0);
-	state.addAnimation(0,"Relax",true,0);
+function render2(delta=0) {
+
+	gl.clearColor(0, 0, 0, 0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	// Apply the animation state based on the delta time.
+	state.update(delta);
+	state.apply(skeleton);
+	skeleton.updateWorldTransform();
+
+	renderer.begin();
+	renderer.drawSkeleton(skeleton, true);
+	renderer.end();
+}
+function exportCanvasAsPNG(fileName) {
+
+    var canvasElement = document.querySelector("canvas");
+
+    var MIME_TYPE = "image/png";
+
+    var imgURL = canvasElement.toDataURL(MIME_TYPE);
+
+    var dlLink = document.createElement('a');
+    dlLink.download = fileName;
+    dlLink.href = imgURL;
+    dlLink.dataset.downloadurl = [MIME_TYPE, dlLink.download, dlLink.href].join(':');
+
+    document.body.appendChild(dlLink);
+    dlLink.click();
+    document.body.removeChild(dlLink);
+}
+function run(maxframes,i){
+	if(i<maxframes){
+		const name = "cqbw_m_"+(("0000"+i).slice(-4));
+		render2(0);
+		exportCanvasAsPNG(name);
+		render2(0.016);
+		setTimeout(function(){run(maxframes,i+1)},500);
 	}
 }
+var lastInteractTime = 0;
 function flip(){
 	skeleton.flipX = !skeleton.flipX;
 }
